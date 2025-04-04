ed7e2d63c097783a5c5514d0fcdda8c5710f73a1
hieuhuynh <hieuhuynhif@gmail.com>
2025-04-04 08:37:53 +0700

    setup  sping

---
+++ src/main/java/org/example/tmstrainingspring/TmsTrainingSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.tmstrainingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsTrainingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsTrainingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tmstrainingspring/TmsTrainingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tmstrainingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsTrainingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
