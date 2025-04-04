34aa6739f6b86be49ef8b2199a0a9a7d8f7f1519
hieuhuynh <hieuhuynhif@gmail.com>
2025-04-04 17:20:11 +0700

    spring

---
+++ src/main/resources/db/migration/V1__Create_Table_User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    age        INT
+);
---
+++ src/main/resources/db/migration/V2__Insert_Initial_Users.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (first_name, last_name, age)
+VALUES ('John', 'Doe', 30),
+       ('Jane', 'Doe', 25);
